e38040be438145eac43722e63e075abe07ae72f0
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2019-06-12 14:51:43 +0530

    Update 56.py

--- 56.py
+++ 56.py
@@ -1,7 +1,12 @@
-a=input()
-if(a.isalpha()):
-    print("no")
-if(a.isnumeric()):
-    print("no")
+q=input()
+r=0
+s=0
+for i in q:
+    if(i.isalpha()==True):
+        r=1
+    elif(i.isdigit()==True):
+        s=1
+if(r==1 and s==1):
+    print("Yes")
 else:
-    print("yes")
+    print("No")
